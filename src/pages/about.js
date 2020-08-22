97351bd56aa859470dacfe1e8985f8d67be2092d
Hugo Licon <hugo_val28@hotmail.com>
2020-08-21 19:18:49 -0600

    add basic navigation

---
+++ src/pages/about.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const About = () => {
+  return <h1>About</h1>
+}
+
+export default About
---
+++ src/pages/reading.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const Reading = () => {
+  return (
+    <Layout>
+      <h1>Reading</h1>
+    </Layout>
+  )
+}
+
+export default Reading
