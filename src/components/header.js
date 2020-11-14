1015f13125c101c88eee0162b25df1130f086dfd
Hugo Licon <hugo_val28@hotmail.com>
2020-11-13 20:39:26 -0700

    fix bug with navigation

--- src/components/header.js
+++ src/components/header.js
@@ -33,7 +33,6 @@ const IconContainer = styled.div`
       transform: ${({ open }) => (open ? "rotate(45deg)" : "rotate(0)")};
     }
     &:nth-child(2) {
-      transform: ${({ open }) => (open ? "translateX(100%)" : "translateX(0)")};
       opacity: ${({ open }) => (open ? 0 : 1)};
     }
     &:nth-child(3) {
