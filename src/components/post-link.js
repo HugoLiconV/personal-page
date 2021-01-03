93290341de9e7a2a5c1a77bfcc746b90c7397a73
Hugo Licon <hugo_val28@hotmail.com>
2021-01-02 20:37:01 -0700

    fix card styles

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -4,19 +4,21 @@ import React from "react"
 const PostLink = ({ post }) => {
   return (
     <article className="card">
-      <Link to={post.frontmatter.path}>
-        {!!post.frontmatter.thumbnail && (
+      {!!post.frontmatter.thumbnail && (
+        <Link to={post.frontmatter.path}>
           <img
             src={post.frontmatter.thumbnail}
             alt={post.frontmatter.title + "- Featured Shot"}
           />
-        )}
-      </Link>
+        </Link>
+      )}
       <header>
+      <div className="post-meta">
+        <span style={{ fontSize: "1rem" }}>{post.frontmatter.date}</span>
+      </div>
         <h2 className="post-title">
           <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
         </h2>
-        <div className="post-meta">{post.frontmatter.date}</div>
       </header>
     </article>
   )
