476d018b52d0ce7bfffa6a83a2eea757b334352c
Hugo Licon <hugo_val28@hotmail.com>
2021-04-19 21:15:18 -0600

    remove blog notice

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -19,38 +19,6 @@ const Blog = ({
     <Layout path={path}>
       <SEO title="Blog" />
       <div className="grids">{Posts}</div>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          padding: 16,
-          textAlign: "center",
-        }}
-      >
-        <img
-          src={Image}
-          alt="Not found"
-          style={{
-            height: "30vh",
-          }}
-        />
-        <h1>Work in progress</h1>
-      </div>
-      <p>
-        I am in progress of copying my blogs from dev.to to this page, if you
-        are interested you can find them{" "}
-        <a
-          href="https://dev.to/hugoliconv"
-          target="_blank"
-          className="underline"
-          rel="noopener noreferrer"
-          onClick={trackClickDevto}
-        >
-          here
-        </a>
-      </p>
     </Layout>
   )
 }
